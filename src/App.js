7ba50864ef3bbba8ecab6c0a7c0fefbf350f1207
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2021-08-10 11:03:40 +0300

    imports fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import anime from 'animejs/lib/anime.es.js';
-import { useEffect } from 'react';
 import LineAnim from './components/lineAnim/lineAnim';
 
 
 function App() {
-  //test
   return (
      <div className="App">
        <LineAnim />
